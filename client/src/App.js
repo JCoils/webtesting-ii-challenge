2e4759bae2ebec9a527a26deef68abaf324ce131
JordanG <jmg236@yahoo.com>
2019-06-25 20:10:09 -0500

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		strike: 0,
+		ball: 0,
+		hit: 0,
+		foul: 0
+	};
+
+	fouls = () => {
+		let player = this.state.foul;
+		this.setState({ foul: player + 1 });
+	};
+
+	hits = () => {
+		let player = this.state.hit;
+		this.setState({
+			hit: player + 1
+		});
+	};
+
+	addStrike = () => {
+		let strikes = this.state.strike;
+		if (strikes < 2) {
+			this.setState({ strike: this.state.strike + 1 });
+		} else {
+			this.setState({ strikes: 0 });
+		}
+	};
+
+	addBall = () => {
+		let balls = this.state.ball;
+		if (balls < 3) {
+			this.setState({ balls: this.state.ball + 1 });
+		} else {
+			this.setState({ balls: 0 });
+		}
+	};
+
+	reset = () => {
+		this.setState({
+			strike: 0,
+			ball: 0,
+			hit: 0,
+			foul: 0
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1> Baseball</h1>
+				<p>Strikes</p>
+				<p>Count: {this.state.strike}</p>
+				<button onClick={this.addStrike}>Strike</button>
+				<p>Balls</p>
+				<p>Count: {this.state.ball}</p>
+				<button onClick={this.addBall}>Ball</button>
+				<p>Hits</p>
+				<p>{this.state.hit}</p>
+				<button onClick={this.hits}>Hits</button>
+				<p>Fouls</p>
+				<p>{this.state.foul}</p>
+				<button onClick={this.fouls}>Foul</button>
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+	it('renders without crashing', () => {
+		render(<App />);
+	});
+
+	it('should display strikes and balls', () => {
+		const { getByText } = render(<App />);
+
+		getByText(/strikes/i);
+
+		getByText(/balls/i);
+	});
+
+	it('should add strikes and balls', () => {
+		const { getAllByText } = render(<App />);
+
+		const button = getAllByText(/strike/i);
+		fireEvent.click(button);
+
+		const button1 = getAllByText(/ball/i);
+		fireEvent.click(button1);
+	});
 });
